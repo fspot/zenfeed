3c0773ddfddc07019369e7f0afd8ed4b57cfa352
fspot <fred@fspot.org>
2014-04-16 01:03:07 +0200

    main method isolated

--- zenfeed/start.py
+++ zenfeed/start.py
@@ -3,26 +3,32 @@
 
 from __future__ import unicode_literals, print_function
 
+# avoid side effects with imports…
 import gevent
 from gevent.monkey import patch_socket
-patch_socket()
 from models import setup_tables, Feed
-setup_tables()
-from app import app
-import views
-from werkzeug.contrib.fixers import ProxyFix
-app.wsgi_app = ProxyFix(app.wsgi_app)
-from deadline_manager import deadlineManager
 
-feeds = Feed.query.all()
-deadlineManager.launch_deadline_workers(feeds)
-deadlineManager.start()
+def main():
+    patch_socket()
+    setup_tables()
+    from app import app
+    import views
+    from werkzeug.contrib.fixers import ProxyFix
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+    from deadline_manager import deadlineManager
 
-if app.config['DEBUG']:
-    app.run(host='0.0.0.0', port=5000)
-else:
-    from gevent.wsgi import WSGIServer
-    http_server = WSGIServer(('0.0.0.0', 5000), app)
-    http_server.serve_forever()
+    feeds = Feed.query.all()
+    deadlineManager.launch_deadline_workers(feeds)
+    deadlineManager.start()
 
-#gevent.joinall([deadlineManager])
+    if app.config['DEBUG']:
+        app.run(host='0.0.0.0', port=5000)
+    else:
+        from gevent.wsgi import WSGIServer
+        http_server = WSGIServer(('0.0.0.0', 5000), app)
+        http_server.serve_forever()
+    #gevent.joinall([deadlineManager])
+
+if __name__ == '__main__':
+    main()
+
